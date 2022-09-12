32c9bd53780f7caa7bd09eb0804dbf59f4630dab
disenofgti <terra.facundo.devacc@gmail.com>
2022-09-12 09:27:01 -0300

    cambios diseño 12.9 v1

--- src/components/estaticas/pagina_cotizar.jsx
+++ src/components/estaticas/pagina_cotizar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const PaginaCotizar =()=>{
     return(
-        <h2>Página Cotizar</h2>
+        <div>
+            <h2>Página
+            <a href="https://wa.me/5401173656656?text=Buenas!%20Estoy%20buscando%20un%20producto%20que%20no%20se%20encuentra%20en%20la%20página"> Cotizar </a> </h2>
+        </div>
     )
 }
 
--- src/components/estaticas/redirecc_apple.jsx
+++ src/components/estaticas/redirecc_apple.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const RedireccionadorApple =({link})=>{
     return(
         <div className="tarjeta_galeria_productos_apple">
-            <img src="https://placekitten.com/590/280"></img>
+            <img src="https://placekitten.com/590/280" className="foto-redirecc-apple"></img>
             <div className="contenedor_organizador">
                     <h2>{link}</h2>
                     <Link to={`/filt/apple/${link.toLowerCase()}`} className="apple_botones_ver_mas">Ver más</Link>
