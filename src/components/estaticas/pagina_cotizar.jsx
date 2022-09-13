2e3a4ae9b33fb1029a13834f3a205507efda6295
disenofgti <terra.facundo.devacc@gmail.com>
2022-09-13 07:11:34 -0300

    cambios diseño 13.9 v1.1

--- src/components/estaticas/pagina_cotizar.jsx
+++ src/components/estaticas/pagina_cotizar.jsx
@@ -4,9 +4,27 @@ import { Link } from "react-router-dom";
 
 const PaginaCotizar =()=>{
     return(
-        <div>
-            <h2>Página
-            <a href="https://wa.me/5401173656656?text=Buenas!%20Estoy%20buscando%20un%20producto%20que%20no%20se%20encuentra%20en%20la%20página"> Cotizar </a> </h2>
+        <div className="pagina-cotizar">
+            <h2>¿Qué necesitas para cotizar tu producto con nosotros?</h2>
+            <p>¿No encontraste eso que buscabas. Llená el formulario con la información necesaria para que te ayudemos a tener lo que buscás.</p>
+            <div className="intro-pagina-cotizar">
+                <div className="sub-cotizar">
+                    <span class=" material-symbols-outlined">link</span>
+                    <h2>Busca el link de tu producto</h2>
+                </div> 
+
+                <div className="sub-cotizar">
+                    <span class="material-symbols-outlined">description</span>
+                    <h2>Completá el formulario con tus datos de contacto</h2>
+                </div>
+                
+                <div className="sub-cotizar">
+                    <span class="material-symbols-outlined">forward_to_inbox</span>
+                    <h2>Envianos tu formulario</h2>
+                </div>
+            </div>
+
+            <iframe src="https://whatsform.com/JjCBWZ" width="100%" height="600" frameBorder="0"></iframe>
         </div>
     )
 }
