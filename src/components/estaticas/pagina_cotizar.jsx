c994109ee894a5ea8378f91c9a0abb77a83fa7ed
disenofgti <terra.facundo.devacc@gmail.com>
2023-01-23 10:28:13 -0300

    Primeros cambios en la rama testeo_estilos del 23/1

--- src/components/estaticas/pagina_cotizar.jsx
+++ src/components/estaticas/pagina_cotizar.jsx
@@ -4,22 +4,24 @@ import React from "react";
 const PaginaCotizar =()=>{
     return(
         <div className="pagina-cotizar">
-            <h2>¿Qué necesitas para cotizar tu producto con nosotros?</h2>
-            <p>¿No encontraste eso que buscabas? Llená el formulario con la información necesaria para que te ayudemos a tener lo que buscás.</p>
+            <h2 className="banner-cotizar">COTIZÁ CON NOSOTROS</h2>
             <div className="intro-pagina-cotizar">
                 <div className="sub-cotizar">
-                    <span className=" material-symbols-outlined">link</span>
-                    <h2>Busca el link de tu producto</h2>
+                    <span className=" material-symbols-outlined angry-animate">link</span>
+                    <h2>ENCONTRÁ LINK DE TU PRODUCTO</h2>
+                    <p>Ajunta el link de tu producto en tu página de confianza para poder calcular los cosotos.</p>
                 </div> 
 
                 <div className="sub-cotizar">
-                    <span className="material-symbols-outlined">description</span>
-                    <h2>Completá el formulario con tus datos de contacto</h2>
+                    <span className="material-symbols-outlined angry-animate">description</span>
+                    <h2>RELLENÁ EL FORMULARIO</h2>
+                    <p>Completa con la información pedida el formulario, para que una vez que coticemos el producto, te llegue toda la información de la cotización.</p>
                 </div>
                 
                 <div className="sub-cotizar">
-                    <span className="material-symbols-outlined">forward_to_inbox</span>
-                    <h2>Envianos tu formulario</h2>
+                    <span className="material-symbols-outlined angry-animate">forward_to_inbox</span>
+                    <h2>ENVIALO</h2>
+                    <p>Finalmente, subí tu formulario, y espera nuestro contacto a la brevedad.</p>
                 </div>
             </div>
 
