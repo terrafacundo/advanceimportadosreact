1abf9824c453151da7f8c48a88f3c25ce6216a09
disenofgti <terra.facundo.devacc@gmail.com>
2023-02-01 16:40:41 -0300

    Ultimo commit

--- src/components/renderindex.jsx
+++ src/components/renderindex.jsx
@@ -9,21 +9,23 @@ import PaginaNosotros from "./estaticas/pagina_nosotros";
 import PaginaGaming from "./estaticas/pagina_gaming";
 import PaginaCotizar from "./estaticas/pagina_cotizar";
 import PreguntasFrec from "./preguntasfrec";
-import BotonesAccesoRapido from "./botones-wpp-ig";
+// import BotonesAccesoRapido from "./botones-wpp-ig";
+// import ScrollToTop from "./scrolltotop.jsx";
 
 const Main = () => {
 
     return(
             <Routes>
-                <Route path='/' element={<Indexadvance/>}/>
-                <Route path='/productos/:id_link' element={<ProductoDetalleExtContainer/>}/>
-                <Route path='/Productos' element={<SeccionBusquedaProductos/>}/>
-                <Route path='/filt/:tipo' element={<SeccionBusquedaProductos/>}/>
-                <Route path='/Apple' element={<PaginaApple/>}/>
-                <Route path='/Nosotros' element={<PaginaNosotros/>}/>
-                <Route path='/Gaming' element={<PaginaGaming/>}/>
-                <Route path='/Cotizar' element={<PaginaCotizar/>}/>
-                <Route path='/PyR' element={<PreguntasFrec/>}/>
+                {/* <ScrollToTop/> */}
+                    <Route path='/' element={<Indexadvance/>}/>
+                    <Route path='/productos/:id_link' element={<ProductoDetalleExtContainer/>}/>
+                    <Route path='/Productos' element={<SeccionBusquedaProductos/>}/>
+                    <Route path='/filt/:tipo' element={<SeccionBusquedaProductos/>}/>
+                    <Route path='/Apple' element={<PaginaApple/>}/>
+                    <Route path='/Nosotros' element={<PaginaNosotros/>}/>
+                    <Route path='/Gaming' element={<PaginaGaming/>}/>
+                    <Route path='/Cotizar' element={<PaginaCotizar/>}/>
+                    <Route path='/PyR' element={<PreguntasFrec/>}/>
             </Routes>
     )
 }
---
+++ src/components/scrolltotop.jsx
@@ -0,0 +1,11 @@
+// import { ScrollRestoration } from "react-router-dom";
+
+// function ScrollToTop() {
+//   return (
+//     <div>
+//       {/* ... */}
+//       <ScrollRestoration />
+//     </div>
+//   );
+// }
+// export default ScrollToTop
